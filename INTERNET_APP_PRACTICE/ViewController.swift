1ecbf7be9208bc2e5231fe9acd63bdb56980eb84
01GOD <GOD@GODS-BOT.local>
2017-03-21 23:53:29 +0800

    THE BASIC APP IS DONE, PAGE IS CENTERED NOW. THE CENTERING ISSUE WAS THE AUTOLAYOUT PARAMS.

--- INTERNET_APP_PRACTICE/ViewController.swift
+++ INTERNET_APP_PRACTICE/ViewController.swift
@@ -9,8 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   
+    @IBOutlet var SHOW_PAGE: UIWebView!
 
     override func viewDidLoad() {
+        
+        let url = URL(string:"http://WWW.ALLONESUN.ORG")
+    
+        SHOW_PAGE.loadRequest(URLRequest(url:url!))
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
