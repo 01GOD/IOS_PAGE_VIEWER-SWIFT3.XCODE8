f1608c48981d8d474754c12834a2380abf30988f
01GOD <GOD@gods-bot.local>
2017-03-21 22:52:15 +0800

    Initial Commit

---
+++ INTERNET_APP_PRACTICE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  INTERNET_APP_PRACTICE
+//
+//  Created by GOD on 3/21/17.
+//  Copyright © 2017 ALL ONE SUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
